14c84de55e8cae80e1ec0f28855f46c5d2572fea
anjia11 <pandjialami21@gmail.com>
2024-01-06 14:21:17 +0700

    nambah status jadwal dokter

--- jadwalDokter.php
+++ jadwalDokter.php
@@ -14,21 +14,29 @@ if (isset($_POST['simpan'])) {
             <script>alert("Hari Tidak Boleh Kosong")</script>
         ';
         echo'meta http-equiv="refresh" content="0>';
+    }
+    elseif($_POST['aktif'] == '999'){
+        echo'
+            <script>alert("Status Tidak Boleh Kosong")</script>
+        ';
+        echo'meta http-equiv="refresh" content="0>';
     }else{
         if (isset($_POST['id'])) {
             $ubah = mysqli_query($mysqli, "UPDATE jadwal_periksa SET 
                                                 hari = '" . $_POST['hari'] . "',
                                                 jam_mulai = '" . $_POST['jam_mulai'] . "',
-                                                jam_selesai = '" . $_POST['jam_selesai'] . "'
+                                                jam_selesai = '" . $_POST['jam_selesai'] . "',
+                                                aktif = '".$_POST['aktif']."'
                                                 WHERE
                                                 id = '" . $_POST['id'] . "'");
         } else {
-            $tambah = mysqli_query($mysqli, "INSERT INTO jadwal_periksa (id_dokter, hari, jam_mulai, jam_selesai) 
+            $tambah = mysqli_query($mysqli, "INSERT INTO jadwal_periksa (id_dokter, hari, jam_mulai, jam_selesai, aktif) 
                                                 VALUES (
                                                     '" . $_SESSION['id_dokter'] . "',
                                                     '" . $_POST['hari'] . "',
                                                     '" . $_POST['jam_mulai'] . "',
-                                                    '" . $_POST['jam_selesai'] . "'
+                                                    '" . $_POST['jam_selesai'] . "',
+                                                    '".$_POST['aktif']."'
                                                 )");
         }
     }
@@ -57,6 +65,7 @@ if (isset($_GET['aksi'])) {
         $hari = '';
         $jam_mulai = '';
         $jam_selesai = '';
+        $aktif = '';
         if (isset($_GET['id'])) {
             $ambil = mysqli_query($mysqli, "SELECT * FROM jadwal_periksa 
                     WHERE id='" . $_GET['id'] . "'");
@@ -64,6 +73,7 @@ if (isset($_GET['aksi'])) {
                 $hari = $row['hari'];
                 $jam_mulai = $row['jam_mulai'];
                 $jam_selesai = $row['jam_selesai'];
+                $aktif = $row['aktif'];
             }
         ?>
             <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>">
@@ -111,6 +121,27 @@ if (isset($_GET['aksi'])) {
                 <input type="time" class="form-control" name="jam_selesai" id="inputJamSelesai" required placeholder="Jam Selesai" value="<?php echo $jam_selesai ?>">
             </div>
         </div>
+        <div class="row">
+            <label for="inputAktif" class="form-label fw-bold">
+                Status
+            </label>
+            <select class="form-control" id="inputAktif" name="aktif">
+                <?php
+                if (!isset($_GET['id'])) {
+                ?>
+                <option value="999" selected>Status Aktif/Tidak</option>
+                <?php
+                }
+                else{
+                ?>
+                    <option value="<?php echo $aktif?>"><?php echo $aktif?></option>
+                <?php
+                    }
+                ?>
+                <option value="Y">Y</option>
+                <option value="N">N</option>
+            </select>
+        </div>
         <div class="row mt-3">
             <div class=col>
                 <button type="submit" class="btn btn-primary rounded-pill px-3 mt-auto" name="simpan">Simpan</button>
@@ -125,10 +156,11 @@ if (isset($_GET['aksi'])) {
         <thead>
             <tr>
                 <th scope="col">#</th>
-                <th scope="col">Id Dokter</th>
+                <th scope="col">Nama Dokter</th>
                 <th scope="col">Hari</th>
                 <th scope="col">Jam Mulai</th>
                 <th scope="col">Jam Selesai</th>
+                <th scope="col">Status</th>
                 <th scope="col">Aksi</th>
             </tr>
         </thead>
@@ -136,16 +168,19 @@ if (isset($_GET['aksi'])) {
         <tbody>
             <!-- Kode PHP untuk menampilkan semua isi dari tabel urut-->
             <?php
-            $result = mysqli_query($mysqli, "SELECT * FROM jadwal_periksa WHERE id_dokter = '".$_SESSION['id_dokter']."'");
+            $result = mysqli_query($mysqli, "SELECT jp.*, dok.nama AS nama_dokter FROM jadwal_periksa AS jp
+                                                    JOIN dokter AS dok ON id_dokter = dok.id
+                                            WHERE id_dokter = '".$_SESSION['id_dokter']."'");
             $no = 1;
             while ($data = mysqli_fetch_array($result)) {
             ?>
                 <tr>
                     <th scope="row"><?php echo $no++ ?></th>
-                    <td><?php echo $data['id_dokter'] ?></td>
+                    <td><?php echo $data['nama_dokter'] ?></td>
                     <td><?php echo $data['hari'] ?></td>
                     <td><?php echo $data['jam_mulai'] ?></td>
                     <td><?php echo $data['jam_selesai'] ?></td>
+                    <td><?php echo $data['aktif'] ?></td>
                     <td>
                         <a class="btn btn-success rounded-pill px-3" href="index.php?page=jadwalDokter&id=<?php echo $data['id'] ?>">Ubah</a>
                         <a class="btn btn-danger rounded-pill px-3" href="index.php?page=jadwalDokter&id=<?php echo $data['id'] ?>&aksi=hapus">Hapus</a>
